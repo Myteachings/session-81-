26db6ab1933e94ff5aeef577918fffde87156e20
Juhi <114214351+Myteachings@users.noreply.github.com>
2022-09-23 19:08:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+function App() {
+    return (
+        <div>
+        <h1>Hi, This is my a Car!</h1>
+
+        <h2>This is my favorite</h2>
+        <img src = {"https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2Fyc3xlbnwwfHwwfHw%3D&w=1000&q=80"} alt=" error loading"/>
+       </div>
+    )
+}
+
+export default App
